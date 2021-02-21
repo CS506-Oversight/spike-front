ddb2a03c4171f2bf99c5828ffabd1bededfa57a3
ofnlut <alonzo.mozee@gmail.com>
2021-02-21 02:28:34 -0600

    Added Reducers/Actions folders, redux-thunk, setup dev-tools

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from "./reducers";
+import { createStore } from 'redux';
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
