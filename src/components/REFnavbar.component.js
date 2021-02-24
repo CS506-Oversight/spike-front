4426bffb2e0316e7c9015bee1c549cf18ed9a018
JonaP11 <jperezrodrig@wisc.edu>
2021-02-23 22:00:37 -0600

    added needed components

--- src/components/REFnavbar.component.js
+++ src/components/REFnavbar.component.js
@@ -8,25 +8,27 @@ export default class Navbar extends Component {
 
     render() {
         return (
+            //For staff and Admin
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand">AutoRack</Link>
+                <Link to="/" className="navbar-brand">A Landing Page</Link>
                 <div className="collapse navbar-collapse">
                 <ul className="navbar-nav mr-auto">
                     <li className="navbar-nav mr-auto">
-                    <Link to="/inventory" className="nav-link">Inventory</Link>
+                    <Link to="/messages" className="nav-link">Update menu(stock-wise)</Link>
                     </li>
                     <li className="navbar-nav mr-auto">
-                    <Link to="/manageMenu" className="nav-link">Manage Menu</Link>
+                    <Link to="/restockPurchases" className="nav-link">Print/Prioritize Orders</Link>
                     </li>
                     <li className="navbar-nav mr-auto">
-                    <Link to="/messages" className="nav-link">Messages</Link>
+                    <Link to="/inventory" className="nav-link">Modify Menu</Link>
                     </li>
                     <li className="navbar-nav mr-auto">
-                    <Link to="/restockPurchases" className="nav-link">Restock RestockPurchases</Link>
+                    <Link to="/manageMenu" className="nav-link">Print Usage Reports</Link>
                     </li>
                 </ul>
                 </div>
             </nav>
         );
     }
+    
 }
