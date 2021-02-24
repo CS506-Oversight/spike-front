3aefb344e7cbf18212f40b5635f04fc9a5294f68
JonaP11 <56172477+JonaP11@users.noreply.github.com>
2021-02-24 04:19:14 -0600

    Update and rename admin-staff-landing.component.js to AdminDashboard.js

---
+++ src/components/AdminDashboard.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Button from "react-bootstrap/Button";
+import Accordion from 'react-bootstrap/Accordion';
+import Card from "react-bootstrap/Card";
+
+function AdminDashboard() {
+
+  return (
+    <Accordion defaultActiveKey="0">
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey="0">
+            Menu
+            </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>Hello! I'm the Menu Info. The Admin should be
+           ble to add/modify menu items and print usage reports.
+              </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey="1">
+            Orders
+            </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="1">
+          <Card.Body>Hello! I'm the Orders Info</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey="2">
+            Pick Ups
+            </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="2">
+          <Card.Body>Hello! I'm the PickUp Info. Receipts should
+           e printed here
+              </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey="3">
+            Settings
+            </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="3">
+          <Card.Body>Hello! I'm the Settings. Admins and staff should
+          be able to save/modify username, password.
+            </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
+}
+
+export default AdminDashboard;
