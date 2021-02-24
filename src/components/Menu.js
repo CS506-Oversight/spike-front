8f47da7dbe0d4803c567024604dcf6620aede19b
jordanpaquin <43942820+jordanpaquin@users.noreply.github.com>
2021-02-24 13:45:51 -0600

    Added cards for menu items

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react';
 import Table from "react-bootstrap/Table";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import CardDeck from 'react-bootstrap/CardDeck';
 
 function Menu() {
     //Can also turn table into Cards instead
     return (
-        <Table striped bordered hover>
+        <CardDeck>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>HamburgerPlaceHolder</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <Button variant="primary"> Add to Cart</Button>
+                </Card.Footer>
+            </Card>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>FriesPlaceHolder</Card.Title>
+                    <Card.Text>
+                        This card has supporting text below as a natural lead-in to additional
+                        content.{' '}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <Button variant="primary">Add to Cart</Button>
+                </Card.Footer>
+            </Card>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>PizzaPlaceHolder</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This card has even longer content than the first to
+                        show that equal height action.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <Button variant="primary">Add to Cart</Button>
+                </Card.Footer>
+            </Card>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>MilkshakePlaceHolder</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This card has even longer content than the first to
+                        show that equal height action.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <Button variant="primary">Add to Cart</Button>
+                </Card.Footer>
+            </Card>
+        </CardDeck>
 
-            <thead>
-            <tr>
-                <th>Item</th>
-                <th>Picture</th>
-                <th>Description</th>
-                <th>Price</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>Fries</td>
-                <td>"Picture Here If Needed"</td>
-                <td>Delicious</td>
-                <td>$1.50</td>
-            </tr>
-            <tr>
-                <td>Burger</td>
-                <td>"Picture Here If Needed"</td>
-                <td>Yummy</td>
-                <td>$2</td>
-            </tr>
-            <tr>
-                <td>Drink</td>
-                <td>"Picture Here If Needed"</td>
-                <td>Any size drink</td>
-                <td>$1</td>
-            </tr>
-            </tbody>
-
-            <h2>
-                Can also be turned into bootstrap Cards
-                instead
-            </h2>
-            
-      </Table>
     );
 } 
 
