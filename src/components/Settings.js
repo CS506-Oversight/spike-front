9fd944d4c40875f64ff53e711fe5007733d314ef
JonaP11 <jperezrodrig@wisc.edu>
2021-02-26 03:20:41 -0600

    Added settings logic component

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,75 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import { useSelector, useDispatch } from 'react-redux';
 
-function Settings() {
+//actions
+import { updateUser } from '../actions/settingsActions'
+
+export default function Settings(props) {
+    const dispatch = useDispatch();
+    const currentUser = useSelector( (state) => state.clientUser.currentUser);
+
+    const[username, setUser] = useState("");
+    const[password, setPassword] = useState("");
+    const[phone, setPhone] = useState("");
+    const[address, setAddr] = useState("");
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        let currentUsername = currentUser.user.username;
+        console.log(currentUsername, username, password, phone, address);
+        let userData = {
+            currentUsername: currentUsername,
+            username: username,
+            password: password,
+            phone: phone,
+            address: address,
+        }
+        dispatch(updateUser(userData));
+
+    };
+    
 
     return (
-        <Form>
-            <br></br>
-            <Form.Group controlId="formBasicOldUser">
-                <Form.Label>Change Username</Form.Label>
-                <Form.Control type="oldUser" placeholder="Enter Old Username" />
-            </Form.Group>
-            <Form.Group controlId="formBasicNewUser">
-                <Form.Control type="newUser" placeholder="Enter New Username" />
-            </Form.Group>
-            <Form.Text className="text-muted">
-                Old username must be correct!
-            </Form.Text>
-            <Button variant="primary" type="submit">
-                Change Username
-            </Button>
+        <Container>
+            <Form onSubmit={e => {handleChange(e)}}>
+                <Form.Group controlId="inlineFormInput">
+                    <Form.Label>Change Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter New Username" value={username} onChange={e => setUser(e.target.value)} />
+                </Form.Group>
 
-            <br></br><br></br><br></br>
-            <Form.Group controlId="formBasicOldPass">
-                <Form.Label>Change Password</Form.Label>
-                <Form.Control type="oldPass" placeholder="Enter Old Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicNewPass">
-                <Form.Control type="newPass" placeholder="Enter New Password" />
-            </Form.Group>
-            <Form.Text className="text-muted">
-                Old password must be correct!
-            </Form.Text>
-            <Button variant="primary" type="submit">
-                Change Password
-            </Button>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Change Password</Form.Label>
+                    <Form.Control type="text" placeholder="Enter New Password" value={password} onChange={e => setPassword(e.target.value)}/>
+                </Form.Group>
 
-            <br></br><br></br><br></br>
-            <Form.Group controlId="formBasicOldNumber">
-                <Form.Label>Change Phone Number</Form.Label>
-                <Form.Control type="oldNumber" placeholder="Enter Old Phone Number" />
-            </Form.Group>
-            <Form.Group controlId="formBasicNewNumber">
-                <Form.Control type="newNumber" placeholder="Enter New Phone Number" />
-            </Form.Group>
-            <Form.Text className="text-muted">
-                Old phone number must be correct!
-            </Form.Text>
-            <Button variant="primary" type="submit">
-                Change Phone Number
-            </Button>
+                <Form.Group controlId="inlineFormInput">
+                    <Form.Label>Change Phone Number</Form.Label>
+                    <Form.Control type="text" placeholder="Enter New Phone Number" value={phone} onChange={e => setPhone(e.target.value)} />
+                </Form.Group>
 
-            <br></br><br></br><br></br>
-            <Form.Group controlId="formBasicOldAddress">
-                <Form.Label>Change Address</Form.Label>
-                <Form.Control type="oldAddress" placeholder="Enter Old Address" />
-            </Form.Group>
-            <Form.Group controlId="formBasicNewAddress">
-                <Form.Control type="newAddress" placeholder="Enter New Address" />
-            </Form.Group>
-            <Form.Text className="text-muted">
-                Old address must be correct!
-            </Form.Text>
-            <Button variant="primary" type="submit">
-                Change Address
-            </Button>
+                <Form.Group controlId="inlineFormInput">
+                    <Form.Label>Change Address</Form.Label>
+                    <Form.Control type="text" placeholder="Enter New Address" value={address} onChange={e => setAddr(e.target.value)} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Update Settings
+                </Button>
 
-            <br></br><br></br><br></br>
-
-        </Form>
+            </Form>
+        </Container>
     );
 } 
-
-export default Settings;
