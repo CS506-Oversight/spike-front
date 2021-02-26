6fde599414bd219758014aadfac8db3a0ab82d53
ofnlut <alonzo.mozee@gmail.com>
2021-02-25 22:07:00 -0600

    github why didn't you say anything

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { SUBMIT_USER, USER_LOGGED_IN } from './types';
-=======
 import { SUBMIT_USER, USER_LOGGED_IN, LOGIN_USER_REQUEST} from './types';
->>>>>>> 34d5e93f3f6869cfe2fb29453e379bbadee44656
 
 export const submitUser = userData => dispatch => {
     console.log('logging in user...' + userData);
@@ -23,12 +19,8 @@ export const submitUser = userData => dispatch => {
                 type: USER_LOGGED_IN,
                 payload: user
             })
-<<<<<<< HEAD
-         ).catch(error => { const errorMsg = error.message});
-=======
          ).catch(error => {
             const errorMsg = error.message;
             console.log(errorMsg);
          });
->>>>>>> 34d5e93f3f6869cfe2fb29453e379bbadee44656
 }
